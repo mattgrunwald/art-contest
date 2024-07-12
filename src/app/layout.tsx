f340c049486881ee8b71b272305921d47803bb22
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-11 20:39:23 -0400

    pretty

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '../components/ThemeProvider'
 
-
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Art Contest',
-  description: "Gem City Art Contest 2024",
+  description: 'Gem City Art Contest 2024',
 }
 
 interface RootLayoutProps {
@@ -18,7 +17,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`bg-slate-50 text-slate-900 antialiased dark:bg-slate-950 dark:text-slate-50 ${inter.className} `}
+        className={`bg-slate-50 text-slate-900 antialiased dark:bg-slate-950 dark:text-slate-50`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <main className="content-width mx-auto px-4 lg:px-8">{children}</main>
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,14 @@
 import { signIn, auth } from '@/auth'
+import Image from 'next/image'
 
 export default async function Page() {
   const session = await auth()
   const email = session === null ? 'NOBODY' : session.user?.email
+  const x = session?.user?.image || ''
   return (
     <>
       <h1>Hello, {email}</h1>
+      <Image src={x} width={500} height={500} alt="you" />
       <form
         action={async () => {
           'use server'
