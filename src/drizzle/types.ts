aa9196a52c7eede717fd062df08abd4806dd0de1
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-17 01:40:41 -0400

    use built-in types

--- src/drizzle/types.ts
+++ src/drizzle/types.ts
@@ -1,47 +1,34 @@
-import { PgTableWithColumns } from 'drizzle-orm/pg-core'
-import { db } from './db' // todo define db outside of index
 import { categories, scores, submissions, users } from './schema'
 import { Role } from './util'
 
-const dummy = async <T extends PgTableWithColumns<any>>(table: T) => {
-  const results = await db.select().from(table)
-  return results[0]
-}
-
-export const dummy2 = async () => {
-  const x = await db.query.users.findFirst({
-    with: {
-      scores: true,
-    },
-  })
-  if (x === undefined) {
-    throw new Error('')
-  }
-  return x
-}
-
-export type DDType = Awaited<ReturnType<typeof dummy2>>
-
-export type DType<T extends PgTableWithColumns<any>> = Awaited<
-  ReturnType<typeof dummy<T>>
->
-
-export type User = DType<typeof users> & {
+export type User = typeof users.$inferSelect & {
   role: Role
 }
-export type Judge = User & Pick<DDType, 'scores'>
-export type Submission = DType<typeof submissions>
+export type Submission = typeof submissions.$inferSelect
 export type ScrubbedSubmission = Pick<
   Submission,
-  'id' | 'grade' | 'level' | 'statement' | 'imageSrc' | 'year'
+  'id' | 'grade' | 'level' | 'statement' | 'imageSrc'
 >
+export type SubmissionForJudge = ScrubbedSubmission & {
+  scores: Score[]
+}
+
+export type SubmissionForAdmin = Submission & {
+  scores: Record<string, Score[]>
+  aggregateScore: string | null
+}
+export type SubmissionForContestant = Pick<
+  Submission,
+  'id' | 'level' | 'imageSrc' | 'statement'
+>
+export type SubmissionForGallery = Pick<Submission, 'id' | 'level' | 'imageSrc'>
 export type UpdateSubmissionDto = Omit<
   Submission,
   'id' | 'userId' | 'createdAt'
 >
-export type Category = DType<typeof categories>
-export type Score = DType<typeof scores>
-export type CreateScoreDto = Omit<Score, 'id'>
+export type Category = typeof categories.$inferSelect
+export type Score = typeof scores.$inferSelect
+export type CreateScoreDto = typeof scores.$inferInsert
 
 export type AdapterReturn<T> =
   | {
@@ -52,3 +39,9 @@ export type AdapterReturn<T> =
       data: null
       error: Error
     }
+
+export type PaginatedResults<T> = {
+  page: number
+  results: T[]
+  total: number
+}
