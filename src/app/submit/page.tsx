79eefdcc36c4e467be6a3ffe0240a555c3b3f702
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-18 01:29:04 -0400

    yeah

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div>Hello</div>
+import { Role } from '@/drizzle/util'
+import { getRoleAndId } from '../serverSideUtils'
+import { notFound, redirect } from 'next/navigation'
+import { DAO } from '@/drizzle/dao'
+import SubmissionForm from '@/components/SubmissionForm'
+
+export default async function Page() {
+  const { role, id } = await getRoleAndId()
+  let editingByAdmin = false
+  console.log(role, id)
+  if (role === Role.Contestant && id) {
+    // try to find their submission
+    const { data, error } = await DAO.readUserSubmission(id)
+
+    if (error !== null) {
+      // todo something
+    }
+    if (data !== null && data !== undefined) {
+      redirect(`/submission/${data.id}`)
+    }
+  } else if (role !== Role.Admin) {
+    return notFound()
+  }
+  return <SubmissionForm />
 }
