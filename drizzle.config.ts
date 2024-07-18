46bbc67e859159f646f975af6d5c636fb6e01c4e
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-17 22:02:57 -0400

    first stab at dockerizing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,11 @@ export default defineConfig({
   schema: './src/drizzle/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    host: process.env.POSTGRES_HOST!,
-    user: process.env.POSTGRES_USER!,
-    password: process.env.POSTGRES_PASSWORD!,
-    database: process.env.POSTGRES_DATABASE!,
-    ssl: true,
+    // host: process.env.POSTGRES_HOST!,
+    // user: process.env.POSTGRES_USER!,
+    // password: process.env.POSTGRES_PASSWORD!,
+    // database: process.env.POSTGRES_DATABASE!,
+    // ssl: process.env.NODE_ENV === 'production',
+    url: process.env.DATABASE_URL!,
   },
 })
---
+++ scripts/local-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+npx drizzle-kit push
+npm run dev
