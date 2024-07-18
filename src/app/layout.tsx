0003646b7c07c89a5cd5e71927ee23e1f811e0e5
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-18 01:24:58 -0400

    add session provider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { ThemeProvider } from '../components/ThemeProvider'
 import { Header } from '@/components/Header'
 import { auth, signIn } from '@/auth'
+import { SessionProvider } from 'next-auth/react'
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -22,8 +23,12 @@ export default async function RootLayout({ children }: RootLayoutProps) {
         className={`bg-slate-50 text-slate-900 antialiased dark:bg-slate-950 dark:text-slate-50`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Header />
-          <main className="content-width mx-auto px-4 lg:px-8">{children}</main>
+          <SessionProvider>
+            <Header />
+            <main className="content-width mx-auto px-4 lg:px-8">
+              {children}
+            </main>
+          </SessionProvider>
         </ThemeProvider>
       </body>
     </html>
