ae42e86da163178b93681c42d9c35982a2775b86
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-18 01:27:14 -0400

    add helpers

---
+++ src/util/helpers.ts
@@ -0,0 +1,17 @@
+import { Level } from '@/drizzle/util'
+
+export const parsePage = (page: string) => {
+  const int = parseInt(page)
+  return isNaN(int) ? 1 : int
+}
+
+export const parseLevel = (lvl: string) => {
+  switch (lvl) {
+    case Level.HighSchool:
+      return Level.HighSchool
+    case Level.MiddleSchool:
+      return Level.MiddleSchool
+    default:
+      return null
+  }
+}
