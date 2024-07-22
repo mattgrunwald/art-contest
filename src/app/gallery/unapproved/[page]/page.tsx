275569a58369a565cdba265f63af5df4c3744090
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-21 21:03:10 -0400

    add page for unapproved submissions

---
+++ src/app/gallery/unapproved/[page]/page.tsx
@@ -0,0 +1,53 @@
+import {
+  AdapterReturn,
+  PaginatedResults,
+  SubmissionForGallery,
+} from '@/db/types'
+import { Level, Role } from '@/db/util'
+import { DAO } from '@/db/dao'
+import { Pager } from '@/components/Pager'
+import { SubmissionGalleryImage } from '@/components/SubmissionGalleryImage'
+import { notFound } from 'next/navigation'
+import { parseLevel, parsePage } from '@/util/helpers'
+import Link from 'next/link'
+import { getRoleAndId } from '@/app/serverSideUtils'
+import { SubmissionFilter } from '@/components/SubmissionFilter'
+
+type GalleryParams = {
+  params: { page: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default async function Page({ params, searchParams }: GalleryParams) {
+  const { role, id } = await getRoleAndId()
+  if (role !== Role.Admin || id == null) {
+    return notFound()
+  }
+  const page = parsePage(params.page)
+  const level = parseLevel(searchParams.level as string) || Level.HighSchool
+
+  const res = await DAO.readUnapprovedSubmissionsForGallery(level, page)
+
+  if (res.error != null) {
+    return <div>ERROR: {res.error.message}</div>
+  }
+
+  const { total, results } = res.data
+
+  return (
+    <>
+      <SubmissionFilter
+        currentLevel={level}
+        showingUnscored={false}
+        showingUnapproved={true}
+        role={role}
+      />
+      <div className="grid grid-cols-2 gap-x-1 gap-y-1">
+        {results.map((sub) => (
+          <SubmissionGalleryImage key={sub.id} sub={sub} />
+        ))}
+      </div>
+      <Pager totalPages={total} page={page} path="/gallery" />
+    </>
+  )
+}
