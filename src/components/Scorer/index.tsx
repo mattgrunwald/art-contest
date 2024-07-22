ec428de59dde728890f85fb6a59c4bf14a89ffd3
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-21 21:44:21 -0400

    remove console logs

--- src/components/Scorer/index.tsx
+++ src/components/Scorer/index.tsx
@@ -12,15 +12,12 @@ export type ScorerProps = {
 export const Scorer = ({ categories, scores, baseScore }: ScorerProps) => {
   const components: JSX.Element[] = []
 
-  console.log('hello?')
   for (const category of categories) {
     const score = scores.find((s) => s.categoryId === category.id)
     let scoreInput: CreateScoreDto
     if (score) {
-      console.log('score found!')
       scoreInput = score
     } else {
-      console.log('score not found..')
       scoreInput = {
         ...baseScore,
         score: null,
