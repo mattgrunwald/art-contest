eb65e9c4b648aed8540dcb34ad03914c0ec5aa9e
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-22 00:21:05 -0400

    expand submission form

---
+++ src/components/SubmissionForm/actions.ts
@@ -0,0 +1,72 @@
+'use server'
+
+import { DAO } from '@/db/dao'
+import { Level } from '@/db/util'
+import { put } from '@vercel/blob'
+import { v4 as uuidv4 } from 'uuid'
+
+const suffixes: Record<string, string> = {
+  'image/png': '.png',
+  'image/jpeg': '.jpg',
+  'image/gif': '.gif',
+  'image/webp': '.webp',
+}
+export async function submit(userId: string, formData: FormData) {
+  const rawFormData = {
+    email: formData.get('email'),
+    name: formData.get('name'),
+    grade: formData.get('grade'),
+    // statement: formData.get('statement'),
+    statement: 'wow aart',
+    imageFile: formData.get('image') as unknown as File,
+  }
+
+  // todo validate none of these are null
+
+  // TODO if admin is creating this, create user too
+
+  const grade = parseInt((rawFormData.grade as string) || '0')
+  const level = grade >= 9 ? Level.HighSchool : Level.MiddleSchool
+
+  const imageFile = rawFormData.imageFile as File
+  const blob = await uploadImage(imageFile)
+
+  if (!blob) {
+    return {
+      data: null,
+      error: 'unknown',
+    }
+  }
+
+  await DAO.createSubmission({
+    userId,
+    grade,
+    level,
+    statement: (rawFormData.statement as string) || '',
+    approved: false,
+    consentForm: null,
+    imageSrc: blob.url,
+  })
+  console.log('success!!!')
+}
+
+async function uploadImage(image: File) {
+  const { size, type } = image
+  // todo check size
+  const suffix = suffixes[type]
+  if (!suffix) {
+    // todo error?
+    console.error(new Error(`unknown file type "${type}"`))
+    return
+  }
+
+  const fileName = `${uuidv4()}${suffix}`
+  try {
+    return await put(fileName, image, {
+      access: 'public',
+    })
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
---
+++ src/components/SubmissionForm/index.tsx
@@ -0,0 +1,139 @@
+'use client'
+
+import { Level } from '@/db/util'
+import { useUser } from '@/hooks/useUser'
+import { File } from 'buffer'
+import { SessionProvider, useSession } from 'next-auth/react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+import { submit } from './actions'
+// import { revalidatePath } from 'next/cache';
+
+interface IFormInput {
+  email: string
+  name: string
+  grade: number
+  statement: string
+  image: File
+}
+
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+export default function SubmissionForm() {
+  const { register, handleSubmit } = useForm<IFormInput>()
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+  const user = useUser()
+  if (!user) {
+    return <div>loading...</div>
+  }
+
+  if (!user.loggedIn) {
+    return <div>You must be logged in to submit!</div>
+  }
+
+  // return (
+  //   <form onSubmit={handleSubmit(onSubmit)}>
+  //     <label>
+  //       Email:
+  //       <input
+  //         disabled={!user.isAdmin}
+  //         {...register('email', {
+  //           required: true,
+  //           // pattern: emailRegex,
+  //         })}
+  //         value={user.isAdmin ? '' : user.email!}
+  //       />
+  //     </label>
+  //     <br />
+
+  //     <label>
+  //       Name:
+  //       <input
+  //         disabled={!user.isAdmin}
+  //         {...register('name', { required: true })}
+  //         value={user.isAdmin ? '' : user.name!}
+  //       />
+  //     </label>
+  //     <br />
+
+  //     <label>
+  //       Grade:
+  //       <input
+  //         type="number"
+  //         min={6}
+  //         max={12}
+  //         {...register('grade', { required: true, min: 6, max: 12 })}
+  //       />
+  //     </label>
+  //     <br />
+  //     <label>
+  //       {"Artist's statement"}
+  //       <textarea {...register('statement', { required: true })} />
+  //     </label>
+  //     <br />
+  //     <label>
+  //       Image (max 4.5MB)
+  //       <input
+  //         type="file"
+  //         {...register('image', { required: true })}
+  //         accept="image/png, image/jpeg, image/gif, image/webp"
+  //       />
+  //     </label>
+  //     <button>Upload</button>
+  //   </form>
+  // )
+
+  if (!user.id) {
+    return <div>no id</div>
+  }
+
+  // todo figure out how to return data from a form submit
+  // so we can navigate to submission page
+  const submitWithId = submit.bind(null, user.id)
+
+  return (
+    <form action={submitWithId}>
+      <label>
+        Email:
+        <input
+          disabled={!user.isAdmin}
+          name="email"
+          value={user.isAdmin ? '' : user.email!}
+        />
+      </label>
+      <br />
+
+      <label>
+        Name:
+        <input
+          disabled={!user.isAdmin}
+          name="name"
+          value={user.isAdmin ? '' : user.name!}
+        />
+      </label>
+      <br />
+
+      <label>
+        Grade:
+        <input type="number" min={6} max={12} name="grade" />
+      </label>
+      <br />
+      <label>
+        {"Artist's statement"}
+        <textarea name="statement" />
+      </label>
+      <br />
+      <label>
+        Image (max 4.5MB)
+        <input
+          // TODO add ref so can check file size before upload
+          type="file"
+          required
+          name="image"
+          accept="image/png, image/jpeg, image/gif, image/webp"
+        />
+      </label>
+      <button>Upload</button>
+    </form>
+  )
+}
