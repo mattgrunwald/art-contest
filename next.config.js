5118d5da253f4a4c9a9dc70720fdf4e850bd2028
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-22 00:19:50 -0400

    allow images from vercel blob

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ module.exports = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'art-contest-pics.public.blob.vercel-storage.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: '2onz6szh6o2ipztz.public.blob.vercel-storage.com',
+        port: '',
+
+      },
     ],
   },
 }
