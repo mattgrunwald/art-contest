9449e12898adaf26d42c1e21c38c711425323005
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-21 20:28:09 -0400

    remove drizzle dir

--- src/drizzle/seeds/fixtures/users/index.ts
+++
@@ -1,9 +0,0 @@
-import { admins } from './admins'
-import { contestants } from './contestants'
-import { judges } from './judges'
-
-export * from './admins'
-export * from './contestants'
-export * from './judges'
-
-export const allUsers = [...admins, ...contestants, ...judges]
