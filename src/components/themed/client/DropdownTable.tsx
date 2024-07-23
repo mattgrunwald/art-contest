4930eec16a102d7ee75d01078b064d5c3518903a
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-22 23:35:04 -0400

    create clientside themed components

---
+++ src/components/themed/client/DropdownTable.tsx
@@ -0,0 +1,34 @@
+'use client'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@headlessui/react'
+import { Table, TableTitle, TableSubtitle, FullTableProps } from '../Table'
+import { ChevronDownIcon } from '@heroicons/react/24/solid'
+
+export const DropdownTable = ({
+  title,
+  subtitle,
+  headers,
+  rows,
+}: FullTableProps) => (
+  <Disclosure as="div" defaultOpen={true}>
+    <DisclosureButton className="group flex items-center justify-between">
+      <span className="text-sm/6 font-medium text-white group-data-[hover]:text-white/80">
+        <TableTitle>{title}</TableTitle>
+      </span>
+      <ChevronDownIcon className="mx-4 size-5 fill-white/60 group-data-[open]:rotate-180 group-data-[hover]:fill-white/50" />
+    </DisclosureButton>
+    <DisclosurePanel className="mt-2 text-sm/5 text-white/50">
+      <>
+        {subtitle && (
+          <div className="pb-4">
+            <TableSubtitle>{subtitle}</TableSubtitle>
+          </div>
+        )}
+        <Table {...{ headers, rows }} />
+      </>
+    </DisclosurePanel>
+  </Disclosure>
+)
