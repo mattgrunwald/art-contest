cfe35b8533878d631beaa0dc077a49daaa4946c0
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-27 17:23:46 -0400

    add tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from 'react'
+import { Tooltip as TT } from 'react-tooltip'
+export type TooltipProps = PropsWithChildren & {
+  content: string
+  id: string
+}
+
+export const Tooltip = ({ id, content, children }: TooltipProps) => {
+  return (
+    <>
+      <span data-tooltip-id={id} data-tooltip-content={content}>
+        {children}
+      </span>
+      <TT id={id} />
+    </>
+  )
+}
