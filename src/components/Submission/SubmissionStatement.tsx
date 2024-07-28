aff06ee5531586a4fe7afb7304205aeb154c6348
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-28 02:44:59 -0400

    center artist statement

--- src/components/Submission/SubmissionStatement.tsx
+++ src/components/Submission/SubmissionStatement.tsx
@@ -3,8 +3,10 @@ export type SubmissionStatementProps = {
 }
 
 export const SubmissionStatement = ({ text }: SubmissionStatementProps) => (
-  <div className="pb-4 pl-4">
-    <div className="pb-2 text-2xl font-bold">{"Artist's Statement"}</div>
-    <p>{text}</p>
+  <div className="flex w-full justify-center">
+    <div className="w-full bg-slate-200 p-2 dark:bg-slate-900 md:w-[650px]">
+      <div className="pb-2 text-2xl font-bold">{"Artist's Statement"}</div>
+      <p>{text}</p>
+    </div>
   </div>
 )
