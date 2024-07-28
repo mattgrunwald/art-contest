4de88f0d59d455acdbe7e4b6e6a107d4620a6562
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-28 16:08:38 -0400

    add const for default input style

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,4 @@
 export const PAGE_SIZE = 36
+
+export const BASE_INPUT_STYLE =
+  'mb-3 mt-3 block w-full rounded-lg border-none bg-slate-200 px-3 py-1.5 text-sm/6 text-slate-950 focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25 dark:bg-slate-800 dark:text-slate-50'
