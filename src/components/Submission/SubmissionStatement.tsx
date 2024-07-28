17a2b089285fd5e11ae28cdd1f1bf7aa7b33d058
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-28 16:10:16 -0400

    prettier statement

--- src/components/Submission/SubmissionStatement.tsx
+++ src/components/Submission/SubmissionStatement.tsx
@@ -3,8 +3,8 @@ export type SubmissionStatementProps = {
 }
 
 export const SubmissionStatement = ({ text }: SubmissionStatementProps) => (
-  <div className="flex w-full justify-center">
-    <div className="w-full bg-slate-200 p-2 dark:bg-slate-900 md:w-[650px]">
+  <div className="mb-4 flex w-full justify-center">
+    <div className="w-full rounded-lg bg-slate-200 p-2 p-4 dark:bg-slate-900 md:w-[650px]">
       <div className="pb-2 text-2xl font-bold">{"Artist's Statement"}</div>
       <p>{text}</p>
     </div>
