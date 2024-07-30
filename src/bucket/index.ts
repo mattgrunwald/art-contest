4a6e776bf40e695f3103d7958cfda571c2d8737c
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-30 19:04:01 -0400

    create module for interacting with bucket storage

---
+++ src/bucket/index.ts
@@ -0,0 +1,27 @@
+import { put, PutBlobResult } from '@vercel/blob'
+import { nanoid } from 'nanoid'
+
+const suffixes: Record<string, string> = {
+  'image/png': '.png',
+  'image/jpeg': '.jpg',
+  'image/webp': '.webp',
+}
+
+export async function uploadImage(
+  image: File,
+): Promise<[string, Promise<PutBlobResult>, null] | [null, null, Error]> {
+  const { type } = image
+  const suffix = suffixes[type]
+  if (!suffix) {
+    const error = new Error(`unknown file type "${type}"`)
+    console.error(error)
+    return [null, null, error]
+  }
+
+  // todo make this an md5 instead
+  const fileName = `${nanoid()}${suffix}`
+  const blobPromise = put(fileName, image, {
+    access: 'public',
+  })
+  return [fileName, blobPromise, null]
+}
