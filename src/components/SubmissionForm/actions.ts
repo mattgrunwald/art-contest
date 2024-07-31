a7ba966148c1309081a201938fc76422e9ae208a
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-30 20:09:16 -0400

    integrate submission upload

--- src/components/SubmissionForm/actions.ts
+++ src/components/SubmissionForm/actions.ts
@@ -2,55 +2,24 @@
 
 import { DAO } from '@/db/dao'
 import { Level } from '@/db/util'
-import { put, PutBlobResult } from '@vercel/blob'
-import { v4 as uuidv4 } from 'uuid'
-import { formSchema } from './formSchema/server'
-import { createHash } from 'node:crypto'
 import { UpdateSubmissionDto } from '@/db/types'
-
-const suffixes: Record<string, string> = {
-  'image/png': '.png',
-  'image/jpeg': '.jpg',
-  'image/gif': '.gif',
-  'image/webp': '.webp',
-}
-
-async function uploadImage(
-  image: File,
-): Promise<[string | null, PutBlobResult | null]> {
-  const { type } = image
-  const suffix = suffixes[type]
-  if (!suffix) {
-    // todo error?
-    console.error(new Error(`unknown file type "${type}"`))
-    return [null, null]
-  }
-
-  const fileName = `${uuidv4()}${suffix}`
-  try {
-    const blob = await put(fileName, image, {
-      access: 'public',
-    })
-    return [fileName, blob]
-  } catch (error) {
-    console.error(error)
-    return [fileName, null]
-  }
-}
+import { newFormSchema, updateFormSchema } from './formSchema/server'
 
 export async function submit(data: FormData): Promise<any> {
   const formData = Object.fromEntries(data)
+  const formSchema = formData.submissionId ? updateFormSchema : newFormSchema
   const parsed = await formSchema.safeParseAsync(formData)
 
   if (!parsed.success) {
+    console.error('failed to parse submission upload', parsed.error)
     return {
       message: 'Invalid form data',
     }
   }
 
-  console.log(parsed.data)
-
   const {
+    name,
+    email,
     submissionId,
     grade,
     statement,
@@ -61,35 +30,11 @@ export async function submit(data: FormData): Promise<any> {
     zip,
     phone,
     image,
+    userId,
   } = parsed.data
-  let userId = parsed.data.userId
   const level = parseInt(grade) >= 9 ? Level.HighSchool : Level.MiddleSchool
 
-  // TODO make user contestant if they are not already
-  if (userId === null) {
-    // TODO if admin is creating this, create user too
-    userId = 'NEW NEW'
-    // await DAO.createUser
-  }
   try {
-    const buffer = await image.arrayBuffer()
-    const imageHash = createHash('md5')
-      .update(Buffer.from(buffer))
-      .digest('hex')
-    // TODO detect if image has changed, upload if it has
-
-    let existingHash = ''
-    if (submissionId) {
-      const { data, error } = await DAO.readSubmission(submissionId)
-      if (error !== null) {
-        return {
-          message: error.message,
-        }
-      } else if (data) {
-        existingHash = data.imageHash
-      }
-    }
-
     const submission = {
       grade,
       level,
@@ -100,33 +45,35 @@ export async function submit(data: FormData): Promise<any> {
       state,
       zip,
       phone,
-      imageHash,
       approved: false,
     } as UpdateSubmissionDto
 
-    if (imageHash !== existingHash) {
-      const [fileName, blob] = await uploadImage(image)
-      // TODO add upload to submittedImages table
-      submission.imageSrc = blob?.url || ''
-      if (!blob) {
+    if (!submissionId) {
+      if (!image) {
         return {
-          message: 'failed to upload image: unknown error',
+          message: 'no image',
         }
       }
-    }
-    if (!submissionId) {
-      await DAO.createSubmission({
-        ...submission,
-        userId,
-        approved: false,
-        consentForm: null,
-      })
+      if (!userId) {
+        const { error } = await createNewUserAndSubmission(
+          submission,
+          name,
+          email,
+          image,
+        )
+      } else {
+        const { error } = await createSubmission(submission, userId, image)
+      }
     } else {
-      await DAO.updateSubmission(submissionId, {
+      if (!userId) {
+        return {
+          message: 'no user id',
+        }
+      }
+      const { error } = await DAO.updateSubmission(userId, submissionId, {
         ...submission,
         approved: false,
-        consentForm: null,
-        updatedAt: new Date(), // todo set timezone here?
+        updatedAt: new Date(),
       })
     }
 
@@ -138,4 +85,38 @@ export async function submit(data: FormData): Promise<any> {
       message: (error as Error).message,
     }
   }
+}
+
+const createNewUserAndSubmission = (
+  submission: UpdateSubmissionDto,
+  name: string,
+  email: string,
+  image: File,
+) => {
+  const userData = {
+    name,
+    email,
+  }
+
+  const subData = {
+    ...submission,
+    approved: false,
+    consentForm: null,
+  }
+  return DAO.createSubmissionAndUser(subData, userData, image)
+}
+
+const createSubmission = (
+  submission: UpdateSubmissionDto,
+  userId: string,
+  image: File,
+) => {
+  const subData = {
+    ...submission,
+    userId,
+    approved: false,
+    consentForm: null,
+  }
+
+  return DAO.createSubmission(subData, image)
 }
