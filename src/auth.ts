7ffe4b9c4276c06c0d36846ba4b4b112434dcaca
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-07-31 14:16:28 -0400

    add authenticators table to auth config

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
-import { accounts, sessions, users } from './db/schema'
+import { accounts, authenticators, sessions, users } from './db/schema'
 import { db } from './db/db'
 import { User } from './db/types'
 import { Role } from './db/util'
@@ -14,6 +14,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     usersTable: users,
     accountsTable: accounts,
     sessionsTable: sessions,
+    authenticatorsTable: authenticators,
   }),
   providers: [
     Google({
