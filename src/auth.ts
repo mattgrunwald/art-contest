e78dbf95f946ff32e1a9182c7fddba30cdc9cec3
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-08-02 13:17:35 -0400

    delete initial role once it isn't needed

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,8 @@ import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import { accounts, authenticators, sessions, users } from './db/schema'
 import { db } from './db/db'
 import { User } from './db/types'
+import { DAO } from './db/dao'
+import { Role } from './db/util'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: {
@@ -18,6 +20,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     Google({
       allowDangerousEmailAccountLinking: true,
+      profile: async (profile) => await addRole(profile),
     }),
   ],
   callbacks: {
@@ -30,3 +33,20 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   // debug: process.env.NODE_ENV !== 'production',
   debug: false,
 })
+
+export const addRole = async (profile: any) => {
+  let updatedProfile = { ...profile, image: profile.picture }
+  const { data, error } = await DAO.readInitialRole(profile.email)
+  if (error) {
+    console.error(`error reading initial role for ${profile.email}`, error)
+  } else {
+    updatedProfile.role = data ? data.role : Role.Contestant
+  }
+  if (data) {
+    const delResult = await DAO.deleteInitialRole(data.id)
+    if (delResult) {
+      console.error(`error deleting initial role for ${profile.email}`)
+    }
+  }
+  return updatedProfile
+}
