8919f6abc70840b0e354cc49dfde11901b5bb70b
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-08-02 12:59:42 -0400

    add ability to read user by email

--- src/db/dao/user.ts
+++ src/db/dao/user.ts
@@ -1,6 +1,8 @@
 import { db } from '@/db/db'
 import { eq } from 'drizzle-orm'
 import { users } from '../schema'
+import { AdapterReturn, User } from '../types'
+import { q, wrap } from './util'
 
 export const deleteUser = async (userId: string) => {
   await db.delete(users).where(eq(users.id, userId))
@@ -10,3 +12,13 @@ export const deleteUser = async (userId: string) => {
 export const deleteAllUsers = async () => {
   await db.delete(users)
 }
+
+export const readUserByEmail = wrap(
+  async (email: string): Promise<AdapterReturn<User | undefined>> => {
+    const user = await q.users.findFirst({
+      where: (users, { eq }) => eq(users.email, email),
+    })
+
+    return { data: user, error: null }
+  },
+)
