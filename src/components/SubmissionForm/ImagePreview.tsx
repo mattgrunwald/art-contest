ee9bb279bbef675261ae24696a6525094e706763
Matt Grunwald <matt.grunwald.dev@gmail.com>
2024-08-04 23:08:16 -0400

    fix distorted preview images

--- src/components/SubmissionForm/ImagePreview.tsx
+++ src/components/SubmissionForm/ImagePreview.tsx
@@ -14,7 +14,11 @@ export const ImagePreview = ({ remoteSrc }: ImagePreviewProps) => {
     <div className="h-[200px] w-full border-2 border-dashed border-white/25">
       {imageSrc ? (
         // eslint-disable-next-line @next/next/no-img-element
-        <img src={imageSrc} className="h-full w-full" alt="Image preview" />
+        <img
+          src={imageSrc}
+          className="h-full w-full translate-x-0 translate-y-0 transform-gpu object-contain"
+          alt="Image preview"
+        />
       ) : remoteSrc ? (
         <Image
           className="h-full w-full translate-x-0 translate-y-0 transform-gpu object-contain"
